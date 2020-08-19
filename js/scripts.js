85d07d3c1bc75b3a087dc20b9fb008404b17c8de
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-19 10:34:03 -0700

    it is a working LeapYear page now
    Co-authored-by: Drew Lockhart <drewmlockhart@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,22 @@ $(document).ready(function(){
   $("#leapYear").submit(function(event) {
     event.preventDefault();
     let year = parseInt($("#year").val());
-    
+    if(isLeapYear(year)===true){
+      $("#yes").show();
+      $("#no").hide();
+    }
+    else if (isLeapYear(year)===false){
+      $("#no").show();
+      $("#yes").hide(); 
+    }
     
   })
-  function isLeapYear(year) {
-    if(year % 100 === 0){
-      return false;
-    }
-    else if (year % 4 === 0) {
+  function isLeapYear(num) {
+    if ((num % 4 === 0) && (num % 100 !== 0) || (num % 400 === 0)) {
       return true;
     } else {
       return false;
     }
   }
-  alert(isLeapYear(2100));
+  
 });
